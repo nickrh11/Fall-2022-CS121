1432f0f882c105bf80fd08dde103cae8cc748a6f
nickrh11 <112117227+nickrh11@users.noreply.github.com>
2022-10-20 14:47:21 -0400

    Add files via upload

---
+++ Week 9 Recursion and Big O/Thursday-Lab/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        BigO example = new BigO();
+
+        example.printOnce(5);
+        example.printNTimes(3);
+        example.printNSquaredTimes(2);
+    }
+}
+
